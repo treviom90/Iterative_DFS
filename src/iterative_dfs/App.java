96e8800a9c8644e22dcb8ca40696eaad640db1c9
Cecilia Treviño <cecilia261002@hotmail.com>
2022-03-07 20:12:32 -0600

    comentarios

--- src/iterative_dfs/App.java
+++ src/iterative_dfs/App.java
@@ -4,22 +4,22 @@ package iterative_dfs;
 public class App {
     
     public static void main(String[] args ){
-        //declaramos lod nodos
+        //declaramos los nodos
         Node node1 = new Node("A");
         Node node2 = new Node("B");
         Node node3 = new Node("C");
         Node node4 = new Node("D");
         Node node5 = new Node("E");
         
-        //hacemos us conexiones 
+        //hacemos sus conexiones 
         node1.addNeighbour(node2);
         node2.addNeighbour(node3);
         node2.addNeighbour(node5);
         node3.addNeighbour(node4);
         
-        //pasamos elnodo objetivo
+        //pasamos el nodo objetivo
         Algorithm algorithm = new Algorithm(node5);
-        //pasaos el nodo origen 
+        //pasamos el nodo origen 
         algorithm.runDeepeningSearch(node1);
     }
 }
